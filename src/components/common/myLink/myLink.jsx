674fd743df0fdd113aaac7a40ff6aa464234716d
Lukas <lukas.ancikevicius@gmail.com>
2021-06-18 11:24:50 +0300

    link active and other styles

--- src/components/common/myLink/myLink.jsx
+++ src/components/common/myLink/myLink.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-
+import linkS from './myLink.module.css';
 class MyLink extends Component {
   state = {};
   render() {
-    return <div>icon pavadinimas</div>;
+    return (
+      <a className={this.props.active ? linkS.active : linkS.link} href="/">
+        <i className={`fa fa-${this.props.icon}`}></i>
+        {this.props.children}
+      </a>
+    );
   }
 }
 
